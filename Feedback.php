137f581c66a595773eb07b3c4112e41c1d79c7a9
User <nikolajevs04@gmail.com>
2025-09-23 10:47:40 +0300

    OOP added

--- Feedback.php
+++ Feedback.php
@@ -1,17 +1,37 @@
 <?php
 class Feedback
 {
+
+    private int $id;
     private string $name;
     private string $email;
     private string $level;
     private string $content;
+    private string $created_at;
 
-    // Konstruktorā tiek nodotas tikai tās vērtības, kuras lietotājs ievada
-    public function __construct(string $name, string $email, string $level, string $content)
+    public function __construct(int $id, string $name, string $email, string $level, string $content, string $created_at)
     {
+        $this->id = $id;
         $this->name = $name;
         $this->email = $email;
         $this->level = $level;
         $this->content = $content;
+        $this->created_at = $created_at;
+    }
+
+
+    public function display()
+    {
+        echo "<h2>Feedback of ID: {$this->id}</h2>";
+        echo "<ul>";
+        echo "<li>Name: {$this->name}</li>";
+        echo "<li>Email: {$this->email}</li>";
+        echo "<li>Level: {$this->level}</li>";
+        echo "<li>Review: {$this->content}</li>";
+        echo "<li>Created At: {$this->created_at}</li>";
+        echo "</ul>";
+        echo "<br><br>";
     }
 }
+
+?>
--- list.php
+++ list.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'Feedback.php';
 
 
 
@@ -17,30 +17,13 @@ try {
     $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
     foreach (new RecursiveArrayIterator($stmt->fetchAll()) as $k => $v) {
-                $feedback= new Feedback($v['name'],$v['email'],$v['level'],$v['content']);
-
-
-        $id = $v['id'];
-        $name = $v['name'];
-        $email = $v['email'];
-        $level = $v['level'];
-        $content = $v['content'];
-        $created_at = $v['created_at'];
-
-        echo "<h2>Feedback of ID:$id</h2>";
-        echo "<ul>";
-        echo " <li>Name: $name</li>";
-        echo " <li>Email: $email</li>";
-        echo  "<li>Level: $level</li>";
-        echo "<li>Review: $content</li>";
-        echo "<li>Created At: $created_at</li>";
-
-        echo "</ul> ";
+        $feedback = new Feedback($v['id'], $v['name'], $v['email'], $v['level'], $v['content'], $v['created_at']);
+        $feedback->display();
 
         echo "<br><br>";
     }
 
-    echo "<a href='index.html'>List Page<a/>";
+    echo "<a href='index.html'>Add<a/>";
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
